ef77010fca4dc7f38e2f5969c8367d77c2eab9f2
waleed46shah <waleed46shah@gmail.com>
2024-07-02 15:38:31 +0500

    Setting up with postgres server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5430,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'almari',
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cartdata } from 'src/entities/CartData';
+import { UserLogin } from 'src/entities/UserLogin';
+import { UserInfo } from 'src/entities/UserInfo';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DBSequenceService } from 'src/dbService.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserLogin, UserInfo, Cartdata])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, DBSequenceService],
 })
 export class UserModule {}
